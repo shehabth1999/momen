b201a0392b995eb7b635635cd259b685157537a6
shehab <shethr999@gmail.com>
2024-07-02 14:14:57 +0300

    added dotenv

--- core/settings.py
+++ core/settings.py
@@ -2,9 +2,13 @@
 using Django 5.0.6.
 
 """
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
